16a908ac7801159166d51e6f40678aefe7bbb01e
Oscar Virot <virot@virot.com>
2024-12-18 20:01:30 +0100

    Prior to FIDO2 Session! Unhandled exception. Yubico.PlatformInterop.SCardException: Encountered an error while attempting to transmit data to a smart card. SCARD_E_NO_ACCESS: Access is denied to the file.    at Yubico.Core.Devices.SmartCard.DesktopSmartCardConnection.Transmit(CommandApdu commandApdu)    at Yubico.YubiKey.SmartCardConnection.SelectApplication()    at Yubico.YubiKey.SmartCardConnection..ctor(ISmartCardDevice smartCardDevice, YubiKeyApplication yubiKeyApplication)    at Yubico.YubiKey.ConnectionFactory.CreateConnection(YubiKeyApplication application)    at Yubico.YubiKey.YubiKeyDevice.Connect(YubiKeyApplication application)    at Yubico.YubiKey.Fido2.Fido2Session..ctor(IYubiKeyDevice yubiKeyDevice)    at Program.<Main>$(String[] args) in C:\GIT-VS\YubiKeyTests\fido2Session_1.12\Program.cs:line 16

---
+++ fido2Session_1.12/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Yubico.YubiKey.Fido2;
+using Yubico.YubiKey;
+using Yubico.Core.Logging;
+using Microsoft.Extensions.Logging;
+
+// Lets get rid of all the noise
+
+Log.ConfigureLoggerFactory(builder => builder.ClearProviders());
+
+// Then the super basic test
+
+Console.WriteLine("Prior to FIDO2 Session!");
+
+var yubiKey = YubiKeyDevice.FindAll().First();
+using (var fido2Session = new Fido2Session(yubiKey))
+{
+    Console.WriteLine("inside FIDO2 Session!");
+}
+
+Console.WriteLine("After FIDO2 Session!");

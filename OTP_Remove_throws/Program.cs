871fec2dfef63ae79797982445ca4d670fd04d56
Oscar Virot <virot@virot.com>
2024-12-25 13:18:38 +0100

    Added test for OTP_Remove Throws An exception was thrown: YubiKey Operation Failed [Warning, state of non-volatile memory is unchanged.]

---
+++ OTP_Remove_throws/Program.cs
@@ -0,0 +1,50 @@
+﻿using Yubico.Core.Logging;
+using Yubico.YubiKey.Otp;
+using Microsoft.Extensions.Logging;
+using Yubico.YubiKey;
+using Yubico.YubiKey.Otp.Operations;
+
+// See https://aka.ms/new-console-template for more information
+
+Slot slot = Slot.ShortPress;
+
+// We dont need the noise
+Log.ConfigureLoggerFactory(builder => builder.ClearProviders());
+
+var yubiKey = YubiKeyDevice.FindAll().First();
+using (var otpSession = new OtpSession(yubiKey))
+{
+    if (slot == Slot.ShortPress) { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsShortPressConfigured}"); }
+    else { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsLongPressConfigured}"); }
+}
+using (var otpSession = new OtpSession(yubiKey))
+{
+    Console.WriteLine($"Setting the content of slot {slot}");
+    ConfigureStaticPassword staticpassword = otpSession.ConfigureStaticPassword(slot);
+    staticpassword.AppendCarriageReturn();
+    staticpassword.WithKeyboard(Yubico.Core.Devices.Hid.KeyboardLayout.sv_SE);
+    staticpassword.SetPassword("password1234".AsMemory());
+    staticpassword.Execute();
+}
+using (var otpSession = new OtpSession(yubiKey))
+{
+
+    if (slot == Slot.ShortPress) { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsShortPressConfigured}"); }
+    else { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsLongPressConfigured}"); }
+    Console.WriteLine($"Time to remove the configuration from {slot}");
+    // Since we didnt set any password DeleteSlot should work
+    try
+    {
+        otpSession.DeleteSlot(slot);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"An exception was thrown: {e.Message}");
+    }
+}
+using (var otpSession = new OtpSession(yubiKey))
+{
+    if (slot == Slot.ShortPress) { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsShortPressConfigured}"); }
+    else { Console.WriteLine($"Configuration state of {slot}: {otpSession.IsLongPressConfigured}"); }
+}
+

ce7f9780424ff01c4ed742288e986a8c92b2fe5c
Oscar Virot <virot@virot.com>
2025-01-26 13:05:49 +0100

    Allow pin entry, support pinUvAuthToken

--- fido2_empty_fido/Program.cs
+++ fido2_empty_fido/Program.cs
@@ -4,6 +4,7 @@ using Yubico.YubiKey;
 using Yubico.Core.Logging;
 using Microsoft.Extensions.Logging;
 using System.Reflection.Metadata;
+using Yubico.YubiKey.Fido2.Commands;
 
 // Lets get rid of all the noise
 
@@ -12,12 +13,35 @@ Log.ConfigureLoggerFactory(builder => builder.ClearProviders());
 // Then the super basic test
 
 var yubiKey = YubiKeyDevice.FindAll().First();
+
+Console.WriteLine($"Found YubiKey with serial number '{yubiKey.SerialNumber}'.");
+Console.WriteLine("Please enter the PIN for the FIDO2 application of the YubiKey:");
+string? userInput = Console.ReadLine();
+
+if (userInput is null || userInput.Length != 6)
+{
+    Console.WriteLine("No PIN entered.");
+    return;
+}
+
+byte[] pin = userInput.Select(c => (byte)c).ToArray();
+
 using (var fido2Session = new Fido2Session(yubiKey))
 {
     int? retries;
     bool? rebootRequired;
-    bool verified = fido2Session.TryVerifyPin((new byte[] { 49,50,51,52,53,54 }).AsMemory(), null, null, out retries, out rebootRequired);
-    Console.WriteLine($"Authenticated succeeded {verified}.");
+    if (fido2Session.AuthenticatorInfo.Options.ContainsKey("pinUvAuthToken"))
+    {
+        Console.WriteLine("PIN UV Auth Token is supported.");
+        bool verified = fido2Session.TryVerifyPin(pin.AsMemory(), PinUvAuthTokenPermissions.CredentialManagement, null, retriesRemaining: out retries, rebootRequired: out rebootRequired);
+        Console.WriteLine($"Authenticated succeeded {verified}.");
+    }
+    else
+    {
+        Console.WriteLine("PIN UV Auth Token is not supported.");
+        bool verified = fido2Session.TryVerifyPin(pin.AsMemory(),null, null, retriesRemaining: out retries, rebootRequired: out rebootRequired);
+        Console.WriteLine($"Authenticated succeeded {verified}.");
+    }
     Console.WriteLine($"FIDO2 version '{fido2Session.AuthenticatorInfo.FirmwareVersion}'.");
     try
     {
